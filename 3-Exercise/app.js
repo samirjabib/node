29c9cfc2901ffd0abff6e6b0f5175696a832bfba
samirjabib <samirdev9669@gmail.com>
2022-12-28 22:23:49 -0500

    add

--- 3-Exercise/app.js
+++ 3-Exercise/app.js
@@ -13,7 +13,6 @@ const express = require('express') /*Creamos express el cual es un framework de 
 
 const app = express() //Iniciamos nuestra API.
 
-
 const database = [
     { 
         id:1,
@@ -32,6 +31,37 @@ app.get('/', (req, res) => {
     res.status(200).json(database) //Recordamos enviar la respuesta en formato .json para que el cliente la pueda interpretar-
 })
 
+
+app.use(express.json()) //Esto es un middleware de express que nos permite convertir de manera automatica el recibir request en formato en json
+
+
+app.post('/',(req, res) => {
+    
+    // database.push ({
+    //     id:3,
+    //     title:'this is my new title',
+    //     is_completed:false,
+    // })
+
+    
+    
+    database.push({
+        id:req.body.id, //Podemos Recibir los datos del body del cliente y agregarlos a la db de manera dinamica, si el cliente pasa un dato que no existe la variable para almacenarlo, o uno de mas, va a tirar un error. 
+        title:req.body.title,
+        is_completed:false,
+    })
+
+
+    res.status(200).json({ //El response es toda la respuesta que vamos a mandar haci.
+       id:3,
+       title:'this is my new todo',
+       is_completed:false,
+       ip:req.ip, //El request termina siendo toda la informacion que tiene un usuario al momento de hacer una peticion
+       verb:req.verb,
+       body : req.body
+    })
+
+})
 
 
 app.listen( 4000, () => { //Ponemos el server en la escucha.
