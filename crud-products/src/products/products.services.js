e537bf54904f844146fa2cd675de46039cd8beff
samirjabib <samirdev9669@gmail.com>
2023-01-04 17:08:39 -0500

    add

--- crud-products/src/products/products.services.js
+++ crud-products/src/products/products.services.js
@@ -1,28 +1,46 @@
+const productControllers = require('./products.controller')
+
+
 const getAllProducts = async(req, res) => {
     try {
-        
+        const products = productControllers.getAllProducts();
+        res.status(200).json(products);
     } catch (error) {
-        
+        res.status(400).json({message:error.message});
     }
 }
 
 
 const createProducts = async(req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
+    const data = req.body
+
+    if (data.name && data.category && data.price && isAvailable) {
+        try {
+            const newProduct = await createProducts(data);
+            res.status(201).json(newProduct);
+        } catch (error) {
+            res.status(400).json({message:error.message});
+        };
+    } else {
+        res.status(404).json({message:'data incompleted'});
+    };
 }
 
 
 const getProductById = async(req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+    const id = req.params.id
+
+    if (id) {
+        try {
+            const product = await getProductById(id);
+            res.status(200).json(product);
+        } catch (error) {
+            res.status(400).json({message:error.message});
+        }
+    } else {
+        res.status(404).json({message:'incorrect id'})
+    };
+};
 
 
 const updateProduct = async(req, res) => {
