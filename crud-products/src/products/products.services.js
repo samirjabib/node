713195f28fc2d1653da34e3f6a2268e431d064f2
samirjabib <samirdev9669@gmail.com>
2023-01-04 16:55:48 -0500

    add

---
+++ crud-products/src/products/products.services.js
@@ -0,0 +1,49 @@
+const getAllProducts = async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+const createProducts = async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+const getProductById = async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+const updateProduct = async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const deleteProduct = async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+module.exports = {
+    getAllProducts,
+    createProducts,
+    getProductById,
+    deleteProduct
+}
