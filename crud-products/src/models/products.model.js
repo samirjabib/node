fbf25bb10e706fd72725b3b017d237ed3619b08f
samirjabib <samirdev9669@gmail.com>
2023-01-04 16:36:28 -0500

    add

--- crud-products/src/models/products.model.js
+++ crud-products/src/models/products.model.js
@@ -1,11 +1,30 @@
-const { Datatypes } = require('sequelize');
+const { DataTypes } = require('sequelize');
 
 const db = require('../utils/database');
 
 const Products = db.define('products', {
     id:{
         primaryKey:true,
-        type:Datatypes.UUID,
+        type:DataTypes.UUID,
         allowNull:false,
     },
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    category:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    price:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+    },
+    isAvailable:{
+        type:DataTypes.BOOLEAN,
+        allowNull:false,
+    }
 })
+
+
+module.exports = Products;
